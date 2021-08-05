a952d47bec90384110d6d6621a12a0004b6d028c
ahsouza <annibalhsouza@gmail.com>
2021-08-05 19:19:47 -0300

    feat: implement new modules e add variables

--- server.go
+++ server.go
@@ -3,16 +3,25 @@ package main
 import ( 
   "log"
   "net"
+  "fmt"
 	"google.golang.org/grpc"
+  "ahsouza-grpc/chat"
 )
 
+const (port = ":8888")
+
 func main() {
-  app, err := net.Listen("tcp", ":8888")
+
+	fmt.Println("Go gRPC Beginners Tutorial!")
+
+  app, err := net.Listen("tcp", port)
   if err != nil {
     log.Fatalf("Failed to listen: %v", err)
   }
-
+  s := chat.Server{}
+  
 	grpcServer := grpc.NewServer()
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(app); err != nil {
 		log.Fatalf("Failed to serve: %s", err)
